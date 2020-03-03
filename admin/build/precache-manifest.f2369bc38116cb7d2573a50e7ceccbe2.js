e4966df7eb9b064c0a547d51e8e67c541b62e9ed
asmakhan534 <asmaahmad9962@gmail.com>
2020-03-03 10:37:24 +0500

    mar 3 changes from local

---
+++ admin/build/precache-manifest.f2369bc38116cb7d2573a50e7ceccbe2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c91db0f9db20dc60f4d68302d7c7d6fc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e9f37b7d14cb631ae3fe",
+    "url": "/static/css/main.5a11a64a.chunk.css"
+  },
+  {
+    "revision": "e44ceec7fa34799fe695",
+    "url": "/static/js/2.0cb1b2f9.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.0cb1b2f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9f37b7d14cb631ae3fe",
+    "url": "/static/js/main.4bb270c8.chunk.js"
+  },
+  {
+    "revision": "127337b8e5d628a68910",
+    "url": "/static/js/runtime-main.a16bc797.js"
+  }
+]);
